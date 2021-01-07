8dd4b754dd7fe107405556ca62731fb23e01fc0d
dzzy6505 <dzzy6505@gmail.com>
2021-01-07 17:12:38 +0900

    add image process

--- run.py
+++ run.py
@@ -5,46 +5,71 @@
 import html2markdown as md
 from bs4 import BeautifulSoup
 import request, sys
+from itertools import zip_longest
 
-if len(sys.argv) == 2:
-   URL = sys.argv[1]
-else: exit()
-
+# if len(sys.argv) == 2:
+#    URL = sys.argv[1]
+# else: exit()
+URL  = 'https://www.acmicpc.net/problem/7576'
+PREFIX_URL = 'https://www.acmicpc.net'
 html = request.get(URL).text
 soup = BeautifulSoup(html, 'html.parser')
 
-
+""" set html title """
 def title(soup):
     title = soup.h1.text.strip()
     title = title[:title.find('\n')]
-    return "# " + title +'\n'
+    title = ['# ' + title]
+    title.append('   \n')
+    return title
 
-
+""" set html body """
 def body(soup):
     problem_body = soup.find('div', {'id': 'problem-body'})
     problem = problem_body.find_all('div', {'class': 'col-md-12'})
 
     md_text = []
     for div in problem:
-        h2 = div.find_all('h2')
+        heads = div.find_all('h2') # h2
+        paragraphs = div.find_all('p') # paragraph
+        samples = div.find_all('pre', {'class':"sampledata"}) #sample data
 
-        if len(h2) >= 2:
-            simple_data = div.find_all('pre', {'class':"sampledata"})
-            for h, data in zip(h2, simple_data):
-                title = h.text[:h.text.find('\n')]
-                data = data.text.strip()
-                md_text.append("## " + title + "\n" )
-                md_text.append("```\n" + data + "\n```" + "\n" )
-        else:
-            title = md.convert(str(div.h2)) + "\n"
-        try:
-            content = div.p.get_text().replace('\xa0', ' ')  + "\n"
-        except :
-            continue
-        md_text.append(title)
-        md_text.append(content)
+        for head, paragraph, sample in zip_longest(heads, paragraphs, samples):
+            if head and not paragraph and not sample:
+                continue
+            if head:
+                try:
+                    head_text = head.text.replace('복사', '').strip()
+                except:
+                    head_text = head.text
+                md_text.append('## ' + head_text )
+                md_text.append('   \n')
+            if paragraph:
+                md_text.append(paragraph.text)
+                md_text.append('   \n')
+                md_text.append('   \n')
+                if paragraph.img:
+                    md_text.append(str(paragraph.img).replace('src="', 'src="'+ PREFIX_URL ))
+                    md_text.append('   \n')
+                    md_text.append('   \n')
+            if sample: #예제 입력
+                sample_in, sample_out = samples
+                md_text.append('```')
+                md_text.append('   \n')
+                md_text.append(sample_in.text)
+                md_text.append('```')
+                md_text.append('   \n')
+
+                md_text.append('```')
+                md_text.append('   \n')
+                md_text.append(sample_out.text)
+                md_text.append('```')
+                md_text.append('   \n')
+
+
     return md_text
 
+""" set html reference """
 def reference(URL):
     line = ["\n"]
     line.append("### 출처\n")
